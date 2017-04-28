361a782725b89a740d4089cb44ab3da67276c733
oztoshiki <e1608@s.akashi.ac.jp>
2017-04-28 22:06:56 +0900

    プログラミングの授業の課題2

--- kadai2-2.c
+++ kadai2-2.c
@@ -2,7 +2,8 @@
 
 int main(int argc,char **argv) {
 	double r,pi=3.1415;
-	scanf("%lf\n",&r);
+
+	scanf("%lf",&r);
 	printf("”¼Œa%lf‚Ì‰~ü‚Ì’·‚³‚Í%lfA–ÊÏ‚Ì‘å‚«‚³‚Í%lf‚Å‚·B",r,2*pi*r,pi*r*r);
 	return 0;
 }
