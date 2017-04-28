bb8e64e9e36b387a5b1fd02b32934f03fed08b7f
oztoshiki <e1608@s.akashi.ac.jp>
2017-04-28 22:19:41 +0900

    プログラミングの授業の課題2

--- kadai2-2.c
+++ kadai2-2.c
@@ -7,7 +7,7 @@ int main(int argc,char **argv) {
 	S=pi*r*r;
 	printf("半径%dの円周の長さは%lf、面積の大きさは%lfです。\n",r,L,S);
 	printf("半径%dの円周の長さは%7lf、面積の大きさは%5lfです。\n",r,L,S);
-	printf("半径%dの円周の長さは%-7.5lf、面積の大きさは%-9.3lfです。\n",r,L,S);
+	printf("半径%dの円周の長さは%-10.5lf、面積の大きさは%-9.3lfです。\n",r,L,S);
 	printf("半径%dの円周の長さは%8.4lf、面積の大きさは%5.4lfです。\n",r,L,S);
 	return 0;
 }
