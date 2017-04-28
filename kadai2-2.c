95ea89bb6326dd91c6de693a472ca3cbaea01045
oztoshiki <e1608@s.akashi.ac.jp>
2017-04-28 22:10:22 +0900

    プログラミングの授業の課題2

--- kadai2-2.c
+++ kadai2-2.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 
 int main(int argc,char **argv) {
-	double r,pi=3.1415;
-
-	scanf("%lf",&r);
-	printf("”¼Œa%lf‚Ì‰~ü‚Ì’·‚³‚Í%lfA–ÊÏ‚Ì‘å‚«‚³‚Í%lf‚Å‚·B",r,2*pi*r,pi*r*r);
+	int r=13;
+	double pi=3.1415;
+	printf("”¼Œa%d‚Ì‰~ü‚Ì’·‚³‚Í%lfA–ÊÏ‚Ì‘å‚«‚³‚Í%lf‚Å‚·B",r,2*pi*r,pi*r*r);
 	return 0;
 }
