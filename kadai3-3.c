0d51dcfeb788190a4787a283666e6595e5e02c0e
oztoshiki <e1608@s.akashi.ac.jp>
2017-05-14 10:34:13 +0900

    課題作成中

--- kadai3-3.c
+++ kadai3-3.c
@@ -3,10 +3,11 @@
 
 int main(int argc,char **argv) {
   char s[10],t[10];
-  int a,b;
+  int a;
+  double b;
   strcpy(s,"130");
-  strcpy(t,"1.56");
+  strcpy(t,"0.0314");
   a=atoi(s);
   b=atof(t);
-  printf("atoi(%s)=%d,atof(%s)=%d\n",s,a,t,b);
+  printf("atoi(%s)=%d,atof(%s)=%f\n",s,a,t,b);
 }
